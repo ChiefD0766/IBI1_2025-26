bc05db84606aceb8afd652ba2f4e2ee84bc0b0bf
ChiefD0766 <qifan.25@intl.zju.edu.cn>
2026-03-09 21:47:54 +0800

    4

---
+++ Practical4/variables.py
@@ -0,0 +1,16 @@
+a=5.08
+b=5.33
+c=5.55
+d=b-a
+e=c-b
+print(d,e)
+#d is larger than e; the population growth is decelerating in Scotland
+X = True
+Y = False
+W = X or Y
+print(W)
+#X     Y     W
+#True  False True
+#True  True  True
+#False True  True
+#False False False
