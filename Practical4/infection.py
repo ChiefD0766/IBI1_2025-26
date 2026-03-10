9b9e21d8a2c13bf91e698c597ae35a6f66f07bff
ChiefD0766 <qifan.25@intl.zju.edu.cn>
2026-03-10 10:30:06 +0800

    5

---
+++ Practical4/infection.py
@@ -0,0 +1,21 @@
+#set total students number to 91
+#set initial infected number to 5
+#set growth rate to 0.4
+#day = 1
+#while infected number is smaller than total students number, calculate new infections for the next day:
+#  infected number*(1+growth rate)
+#  day+1
+#  print day and infected number
+#print the final day numbers
+total_students = 91
+initial_infected = 5
+growth_rate = 0.4
+infected = initial_infected
+day = 1
+print("Day",day,infected,"infected students")
+while infected < total_students:
+    infected = infected*(1+growth_rate)
+    day += 1
+    print("Day",day,infected,"infected students")
+
+print("All students are infected after",day,"days.")
