6e8b204cae323c094c108300eace67173fe94438
ChiefD0766 <qifan.25@intl.zju.edu.cn>
2026-03-11 13:39:00 +0800

    edit

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,7 +3,12 @@ b=5.33
 c=5.55
 d=b-a
 e=c-b
-print(d,e)
+if d>e:
+    print("d is larger than e")
+if e>d:
+    print("e is larger than d")
+if e==d:
+    print("e is equal to d")
 #d is larger than e; the population growth is decelerating in Scotland
 X = True
 Y = False
